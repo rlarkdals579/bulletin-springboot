89718b7991810a696cad86b32bba5b824b0e9028
kangmin <rlarkdals579@naver.com>
2021-09-30 06:30:08 -0400

    Post function Ver.01 , Total List of views

--- src/main/java/com/kangmin/book/springboot/service/PostsService.java
+++ src/main/java/com/kangmin/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.kangmin.book.springboot.service;
 
 import com.kangmin.book.springboot.domain.posts.Posts;
 import com.kangmin.book.springboot.domain.posts.PostsRepository;
+import com.kangmin.book.springboot.web.dto.PostsListResponseDto;
 import com.kangmin.book.springboot.web.dto.PostsResponseDto;
 import com.kangmin.book.springboot.web.dto.PostsSaveRequestDto;
 import com.kangmin.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -39,4 +43,10 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }
