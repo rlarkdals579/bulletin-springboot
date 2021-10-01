88f1274081632a889690fc2f23f826082ca8eb87
kangmin <rlarkdals579@naver.com>
2021-10-01 15:21:00 -0400

    Post function Ver.2 - Update,Edit

--- src/main/java/com/kangmin/book/springboot/web/IndexController.java
+++ src/main/java/com/kangmin/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.kangmin.book.springboot.web;
 
 import com.kangmin.book.springboot.service.PostsService;
+import com.kangmin.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,11 +17,22 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
+
         return "posts-save";
+    }
+
+    @GetMapping
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+
     }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,28 +1,62 @@
 var main = {
-    init : function () {
+    init: function () {
         var _this = this;
-        $('#btn-save').on('click', function (){
+
+        // When a click event occurs in an html element with an id called btn-save,
+        // register the event to execute the save function.
+        $('#btn-save').on('click', function () {
             _this.save();
         });
+        // When a click event occurs in an html element with an id called btn-update,
+        // register the event to execute the update function.
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
     },
-    save :  function (){
+
+    // save function triggered by 'SAVE' button
+    save: function () {
         var data = {
-            title : $('#title').val(),
-            author : $('#author').val(),
-            content : $('#content').val()
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
         };
         $.ajax({
             type: 'POST',
             url: '/api/v1/posts',
             dataType: 'json',
-            contentType:'application/json; charset=utf-8',
+            contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
-        }).done(function() {
+        }).done(function () {
             alert('Successfully Posted.');
             window.location.href = '/'; // if successfully posted, go back to the main page(/)
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     },
+
+    // update function triggered by 'UPDATE' button
+    update: function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id, // add id to URL to distinguish which post have to edited
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('Successfully Edited.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
 };
+
 main.init();
