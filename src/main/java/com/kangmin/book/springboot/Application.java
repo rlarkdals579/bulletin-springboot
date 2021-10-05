825b2e5da1f8e3b9c7aa4b98827a257354d2daf1
kangmin <rlarkdals579@naver.com>
2021-10-05 10:44:21 -0400

    Google Login Ver.2.01

--- src/main/java/com/kangmin/book/springboot/Application.java
+++ src/main/java/com/kangmin/book/springboot/Application.java
@@ -3,12 +3,14 @@ package com.kangmin.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing //Activating JPA Auditing functions
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.out.println("Spring Core Version:- " + SpringVersion.getVersion());
         SpringApplication.run(Application.class, args);
     }
 }
