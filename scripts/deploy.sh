f99776915851f69cf9bd58a4a9d12a2640be66f8
kangmin <rlarkdals579@naver.com>
2021-10-22 19:01:50 -0400

    travis.CI - automated deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=freelec-springboot2-webservice
+
+echo "> Copy Build file"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> Checking current application's PID"
+
+CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+
+echo "Current application PID ": $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> There is no current running application. Exit denied"
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> Deploying new application"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME : permit access"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME execute"
+
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
